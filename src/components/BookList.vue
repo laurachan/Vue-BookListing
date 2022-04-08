28f2e9ec0b59b6cbd68d507a1d882c6718dfe825
Laura Chan <lkchan@uvic.ca>
2022-04-08 16:56:00 -0700

    Implemented BookForm

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,14 +4,17 @@
         <ul>
             <book-item v-for="book in books" :key="book.title" :book="book">{{ book.title }}: {{ book.author }}</book-item>
         </ul>
+        <book-form @addBook="appendBook" />
     </div>
 </template>
 
 <script>
 import BookItem from "./BookItem";
+import BookForm from "./BookForm";
+
 export default {
     name: "BookList",
-    components: { BookItem },
+    components: { BookItem, BookForm },
     data() {
         return {
             title: "All Books",
@@ -20,6 +23,11 @@ export default {
                 {title: 'American Gods', author: 'Neil Gaiman'},
                 {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
             ]
+        }
+    },
+    methods: {
+        appendBook(bookTitle, bookAuthor) {
+            this.books.push({ title: bookTitle, author: bookAuthor });
         }
     }
 }
